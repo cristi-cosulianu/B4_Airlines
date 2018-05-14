3f2dbe4f5609039e89358d4e35d7ae62343d71f3
cristi-cosulianu <cristi_cosulianu@yahoo.com>
2018-05-14 23:32:38 +0300

    Flights base structure

--- Gateway/src/main/webapp/app/flights-page/flights-page.component.ts
+++ Gateway/src/main/webapp/app/flights-page/flights-page.component.ts
@@ -5,7 +5,9 @@ import { TicketModel } from '../models/ticket-model';
 @Component({
   selector: 'jhi-flights-page',
   templateUrl: './flights-page.component.html',
-  styles: []
+  styleUrls: [
+    './flights-page.component.css'
+]
 })
 export class FlightsPageComponent implements OnInit {
   public ticket = new TicketModel();
@@ -16,5 +18,12 @@ export class FlightsPageComponent implements OnInit {
   ngOnInit() {
     this.data.ticketInfo.subscribe((_data) => this.ticket = _data);
     this.data.updateTicket(this.ticket);
+  }
+
+  sendSubmision() {
+    const departure = (<HTMLInputElement>document.getElementById('departure')).value;
+    const destination = (<HTMLInputElement>document.getElementById('destination')).value;
+    console.log(departure);
+    console.log(destination);
   }
 }
