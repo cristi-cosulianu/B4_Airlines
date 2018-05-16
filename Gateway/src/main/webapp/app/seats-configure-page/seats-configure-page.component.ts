2d7db3bd94ef365441f8d8acff923dee5f8a1c15
Mihai11 <mihai.samson@yahoo.com>
2018-05-17 01:19:23 +0300

    added new query

--- Gateway/src/main/webapp/app/seats-configure-page/seats-configure-page.component.ts
+++ Gateway/src/main/webapp/app/seats-configure-page/seats-configure-page.component.ts
@@ -13,10 +13,10 @@ export class SeatsConfigurePageComponent implements OnInit {
 
   public numbers0to107: Array<number> = new Array<number>();
   private chosenSeats: Array<number> = new Array<number>();
-  private occupiedSeats: Array<number> = [0, 6, 12, 18, 24, 30, 36, 42, 48, 54, 60, 66, 72, 78, 84, 90, 96, 102];
+  private occupiedSeats: Array<number> = new Array<number>();
   public ticket = new TicketModel();
   public indexSeat = 1;
-  // private id_flight: string = '123MV';
+  private id_flight: String = '123mv';
   // private planeType: number = 1;
   private seat: Seats;
   constructor(private data: DataService, private service: SeatsService) {
@@ -25,14 +25,6 @@ export class SeatsConfigurePageComponent implements OnInit {
 
   modulo6(index) {
     if (index % 6 === 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  modulo3(index) {
-    if (index % 3 === 0) {
       return true;
     } else {
       return false;
@@ -57,6 +49,12 @@ export class SeatsConfigurePageComponent implements OnInit {
       this.numbers0to107.push(i);
     }
 
+    this.service.query({ id_flight: this.id_flight }).subscribe((data) => {
+      console.log(data);
+      for (let i = 0; i < data.body.length; i++) {
+        this.occupiedSeats.push(data.body[i].seat_index);
+      }
+    });
     // this.createSpaces();
   }
 
@@ -88,43 +86,16 @@ export class SeatsConfigurePageComponent implements OnInit {
   }
 
   saveSeats() {
-    // private occupiedSeats: Array<number> = [1, 7, 13, 19, 25, 31, 37, 43, 49, 55, 61, 67, 73, 79, 85, 91, 97, 103];
-    // this.seat = new Seats(1, 12, this.id_flight.toString(), 1);
-    this.seat = new Seats();
-    this.seat.id_flight = '123mv';
-    this.seat.type = 2;
-    this.seat.seat_index = 10;
-    this.service.create(this.seat).subscribe();
-    // this.seat.loc_index = 7;
+    // this.seat = new Seats();
+    // this.seat.id_flight = '123mv';
+    // this.seat.type = 3;
+    // this.seat.seat_index = 0;
     // this.service.create(this.seat).subscribe();
-    // this.seat.loc_index = 13;
-    // this.service.create(this.seat).subscribe();
-    // this.seat.loc_index = 19;
-    // this.service.create(this.seat).subscribe();
-    // this.seat.loc_index = 31;
-    // this.service.create(this.seat).subscribe();
-    // this.seat.loc_index = 37;
-    // this.service.create(this.seat).subscribe();
-    // this.seat.loc_index = 43;
-    // this.service.create(this.seat).subscribe();
-    // this.seat.loc_index = 49;
-    // this.service.create(this.seat).subscribe();
-    // this.seat.loc_index = 55;
-    // this.service.create(this.seat).subscribe();
-    // this.seat.loc_index = 61;
-    // this.service.create(this.seat).subscribe();
-    // this.seat.loc_index = 67;
-    // this.service.create(this.seat).subscribe();
-    // for(let i=0; i < this.chosenSeats.length; i++){
-    // this.seat.seat_index=this.chosenSeats[i];
-    // this.seat.id=i+2;
-    // this.service.create(this.seat).subscribe();
-    // }
     // this.service.delete()
 
     // this.service.query('id_flight=123mv').subscribe();
-    this.service.find(952).subscribe((data) => {
-      console.log(data);
-    });
+    // this.service.find(952).subscribe((data) => {
+    //   console.log(data);
+    // });
   }
 }
