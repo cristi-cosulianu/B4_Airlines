2a0a0d9351be17026a58e72250a2d11a0eea1d64
cristi-cosulianu <cristi_cosulianu@yahoo.com>
2018-05-17 22:45:42 +0300

    TSLint + TypeScript v.2.3.4

--- Gateway/src/main/webapp/app/flights-page/flights-page.component.ts
+++ Gateway/src/main/webapp/app/flights-page/flights-page.component.ts
@@ -19,7 +19,7 @@ export class FlightsPageComponent implements OnInit {
     this.data.ticketInfo.subscribe((_data) => this.ticket = _data);
     this.data.updateTicket(this.ticket);
     this.http.get('http://localhost:8050/flights').subscribe((data) => {
-      this.createRowsUsingData(data,"some");
+      this.createRowsUsingData(data, 'some');
     });
   }
 
@@ -28,117 +28,112 @@ export class FlightsPageComponent implements OnInit {
     const destination = (<HTMLInputElement>document.getElementById('destination')).value;
     this.http.get('http://localhost:8050/flights/' + departure + '/' + destination).subscribe((data) => {
       this.removeAllRows();
-      this.createRowsUsingData(data,"all");
+      this.createRowsUsingData(data, 'all');
     });
   }
 
-  removeAllRows(){
+  removeAllRows() {
     // Get parent container <tbody>.
     const tableBody = document.getElementById('tableBody');
-
     // Get all rows.
     const rows = document.getElementsByClassName('tableRow');
-    for(let iterator = 0;iterator < rows.length; iterator++){
+    for (let iterator = 0; iterator < rows.length; iterator++) {
       // Remove row by row.
       tableBody.removeChild(rows[iterator]);
     }
   }
 
-  createRowsUsingData(data, howMany){
+  createRowsUsingData(data, howMany) {
     // Get parent container <tbody>.
     const tableBody = document.getElementById('tableBody');
 
       let size;
-      if(howMany === "some"){
+      if (howMany === 'some') {
         size = 10;
       } else {
         size = Object.keys(data).length;
       }
 
       // Iterate through server response.
-      for (let iterator = 0;iterator < size; iterator++){
+      for (let iterator = 0; iterator < size; iterator++) {
         // Select one element.
-        const obj = data[iterator];
-
-        // Create a new row and new columns.
-        let row = this.createElement('tr');
-        row.setAttribute('class','tableRow');
-        let flightId = this.createElement('td');
-        let departure = this.createElement('td');
-        let destination = this.createElement('td');
-        let price = this.createElement('td');
-        let availableSeats = this.createElement('td');
-        let company = this.createElement('td');
-        let rating = this.createElement('td');
-        let reviews = this.createElement('td');
-
-        // Assign text to simple columns.
-        flightId.innerText = obj.id;
-        departure.innerText = obj.departure;
-        destination.innerText = obj.arrival;
-        price.innerText = obj.priceRangeMin;
-        availableSeats.innerText = obj.avaibleSeats;
-        company.innerText = obj.company;
-
-        // Assign text and $ sign to price column.
-        let span = this.createElement('span');
-        span.setAttribute('class','price');
-        span.innerText = '$';
-        price.appendChild(span);
-
-        // Create div container for raiting stars.
-        let ratingDiv = this.createElement('div');
-        ratingDiv.setAttribute('class','star-rating');
-        for(let i=0;i<5;i++) {
-          // Create stars.
-          let span = this.createElement('span');
-
-          // Assign to stras to be yellow or unfilled.
-          if(i<obj.rating) { span.setAttribute('class','fa fa-star'); }
-          else { span.setAttribute('class','fa fa-star-o'); }
-
-          switch (i){
-            case 0: span.setAttribute('data-rating','1'); break;
-            case 1: span.setAttribute('data-rating','2'); break;
-            case 2: span.setAttribute('data-rating','3'); break;
-            case 3: span.setAttribute('data-rating','4'); break;
-            case 4: span.setAttribute('data-rating','5'); break;
+        if (data[iterator] !== undefined) {
+          const obj = data[iterator];
+          // Create a new row and new columns.
+          const row = this.createElement('tr');
+          row.setAttribute('class', 'tableRow');
+          const flightId = this.createElement('td');
+          const departure = this.createElement('td');
+          const destination = this.createElement('td');
+          const price = this.createElement('td');
+          const availableSeats = this.createElement('td');
+          const company = this.createElement('td');
+          const rating = this.createElement('td');
+          const reviews = this.createElement('td');
+          // Assign text to simple columns.
+          flightId.innerText = obj.id;
+          departure.innerText = obj.departure;
+          destination.innerText = obj.arrival;
+          price.innerText = obj.priceRangeMin;
+          availableSeats.innerText = obj.avaibleSeats;
+          company.innerText = obj.company;
+          // Assign text and $ sign to price column.
+          const dolar = this.createElement('span');
+          dolar.setAttribute('class', 'price');
+          dolar.innerText = '$';
+          price.appendChild(dolar);
+          // Create div container for raiting stars.
+          const ratingDiv = this.createElement('div');
+          ratingDiv.setAttribute('class', 'star-rating');
+          for (let i = 0; i < 5; i++) {
+            // Create stars.
+            const span = this.createElement('span');
+            // Assign to stras to be yellow or unfilled.
+            if (i < obj.rating) {
+              span.setAttribute('class', 'fa fa-star');
+            } else {
+              span.setAttribute('class', 'fa fa-star-o');
+            }
+            switch (i) {
+              case 0: span.setAttribute('data-rating', '1'); break;
+              case 1: span.setAttribute('data-rating', '2'); break;
+              case 2: span.setAttribute('data-rating', '3'); break;
+              case 3: span.setAttribute('data-rating', '4'); break;
+              case 4: span.setAttribute('data-rating', '5'); break;
+            }
+            ratingDiv.appendChild(span);
           }
-          ratingDiv.appendChild(span);
+          rating.appendChild(ratingDiv);
+          // Create button for reviews.
+          const button = this.createElement('button');
+          button.setAttribute('class', 'btn btn-primary btn-sm');
+          button.setAttribute('type', 'button');
+          button.setAttribute('data-toggle', 'collapse');
+          button.setAttribute('data-target', '#collapseReview');
+          button.setAttribute('aria-expanded', 'false');
+          button.setAttribute('aria-controls', 'collapseReview');
+          button.innerText = 'View';
+          reviews.appendChild(button);
+          // Add all columns to new created row.
+          row.appendChild(flightId);
+          row.appendChild(departure);
+          row.appendChild(destination);
+          row.appendChild(price);
+          row.appendChild(availableSeats);
+          row.appendChild(company);
+          row.appendChild(rating);
+          row.appendChild(reviews);
+          // Add row to table body.
+          tableBody.appendChild(row);
         }
-        rating.appendChild(ratingDiv);
-
-        // Create button for reviews.
-        let button = this.createElement('button');
-        button.setAttribute('class','btn btn-primary btn-sm');
-        button.setAttribute('type','button');
-        button.setAttribute('data-toggle','collapse');
-        button.setAttribute('data-target','#collapseReview');
-        button.setAttribute('aria-expanded','false');
-        button.setAttribute('aria-controls','collapseReview');
-        button.innerText = 'View';
-        reviews.appendChild(button);
-
-        // Add all columns to new created row.
-        row.appendChild(flightId);
-        row.appendChild(departure);
-        row.appendChild(destination);
-        row.appendChild(price);
-        row.appendChild(availableSeats);
-        row.appendChild(company);
-        row.appendChild(rating);
-        row.appendChild(reviews);
-
-        // Add row to table body.
-        tableBody.appendChild(row);
       }
   }
 
-  createElement(elementType){
+  createElement(elementType) {
     // Create new element.
-    let element = document.createElement(elementType);
+    const element = document.createElement(elementType);
     // I don't know what is that but is needed.
-    element.setAttribute('_ngcontent-c0','');
+    element.setAttribute('_ngcontent-c0', '');
     return element;
   }
 }
