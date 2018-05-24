fb72164ab27bf461662134d3287c9c7ce4642ad8
Alex ln <logalex96@gmail.com>
2018-05-24 15:46:56 +0300

    update payment page

--- Gateway/src/main/webapp/app/payment-page/payment-page.component.ts
+++ Gateway/src/main/webapp/app/payment-page/payment-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ITicket } from './tikerPassengerInfo.interface';
 
 @Component({
   selector: 'jhi-payment-page',
@@ -8,10 +10,90 @@ import { Component, OnInit } from '@angular/core';
 
 export class PaymentPageComponent implements OnInit {
 
+  showInfoForm: boolean = false;
+  ticketPrice: number = 100;
+  taxesPrice: number = 85.64;
+  insurancePrice: number = 7.68;
+  discountPromo: number = 5;
+  totalPrice: number = this.ticketPrice + this.taxesPrice + this.insurancePrice - this.discountPromo;
+
+  flightInfos: ITicket[] = [{
+    flightDate: "29/02/1996",
+    departLocation: "London",
+    landLocation: "Bucharest",
+    stops: 1,
+    departTime: "16:30",
+    landTime: "18:30"
+  }]
+
+  getText: any[] = [{
+    "title": 'Fill in the form below',
+    "subTitle": 'To​​ comply with the TSA Secure Flight program, the traveler information listed here must exactly match the information on the government - issued photo ID that the traveler presents at the airport.',
+    "form": [
+      {
+        "title": 'Passengers',
+      },
+      {
+        "title": 'Passenger contact information',
+        "subTitle": 'Please let us know the best way to reach you during travel, for important flight status updates or notifications. Our team will use this information to contact you with updates about your travel, if necessary.'
+      },
+      {
+        "title": 'Special passenger needs (Optional)',
+        "optionsCheckbox": [
+          { "id": 1, "type": 'Blind' },
+          { "id": 2, "type": 'Deaf' },
+          { "id": 3, "type": 'Congnitive disability' },
+          { "id": 4, "type": 'Other disability requiring assistance' },
+          { "id": 5, "type": 'Service animal' }
+        ]
+      },
+      {
+        "title": 'Payment',
+        "optionsRadio": [
+          { "id": 1, "type": 'Credit card' },
+          { "id": 2, "type": 'Debit card' },
+          { "id": 3, "type": 'Paypal' },
+        ]
+      }
+    ],
+    "formID": [{
+      "firstName": 'First name',
+      "lastName": 'Last name',
+      "middleName": 'Middle name',
+      "sex": 'Sex',
+      "date": 'Date of birth'
+    }],
+    "formCard": [{
+      "name": 'Name on card',
+      "number": 'Credit card number',
+      "exp": 'Expiration',
+      "cvv": 'CVV'
+    }],
+    "checkCart": [{
+      "ticket": 'Ticket (' + 1 + ')',
+      "taxes": 'Taxes and fees',
+      "discount": 'First time discount',
+      "insurance": 'Insurance',
+      "total": 'Total',
+      "totalSmall": 'Including taxes and fees',
+      "departed" : 'Departed'
+    }]
+  }]
+
   constructor() { }
 
   ngOnInit() {
     
+  }
+
+  toggleInfoForm(selectedValue): void {
+    this.showInfoForm = selectedValue;
+  }
+
+  firstName = '';
+
+  onKey(event: any) {
+    this.firstName = event.target.value;
   }
 
 }
---
+++ Gateway/src/main/webapp/app/payment-page/tikerPassengerInfo.interface.ts
@@ -0,0 +1,8 @@
+export interface ITicket {
+    flightDate: string,
+    departLocation: string,
+    landLocation: string,
+    stops: number,
+    departTime: string,
+    landTime: string
+}
